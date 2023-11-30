87c6530ab465423579b41e505cfce2db540b8d5c
Sergio Ruiz <sergioruizrock@gmail.com>
2023-11-30 03:38:46 -0300

    se realizaron las validaciones de los input por js y algunas correcciones

--- js/administrador.js
+++ js/administrador.js
@@ -1,33 +1,87 @@
 import { Pelicula } from "./classPelicula.js";
-let newPelicula
-const crearPelicula=()=>{
+
+const codigo=document.getElementById("codigo"),
+    titulo=document.getElementById("titulo"),
+    descripcion=document.getElementById("descripcion"),
+    url=document.getElementById("url"),
+    genero=document.getElementById("genero");
+    
+    console.log(codigo + "  asdasddsa");
+
+const modalPelicula=new bootstrap.Modal(document.getElementById("modalPelicula"))
+
+const listPeliculas=JSON.parse(localStorage.getItem("keyListaPeliculas")) || [] 
+//si JSON.Parse es null entonces se crea el array vacio "[]"
+
+const abrirModalPelicula=()=>{{
+  modalPelicula.show();
+}}
+
+
+const crearPelicula=(e)=>{
     //tomamos los datos y creamos el objeto
-    let form=document.querySelector('.formAgregarPelicula')
-    console.log(form);
-    let campos=form.elements
-    let codigo=campos[0].value
-    let titulo=campos[1].value
-    let descripcion=campos[2].value
-    let url=campos[3].value
-    let genero=campos[4].value
+    e.preventDefault();
 
-    newPelicula = new Pelicula(codigo,titulo,descripcion,url,genero)
+    if(titulo.value.length>20 || titulo.value.length<=1){
+      alert("ponele titulo!!!");
+      return;
+    }
+    if(descripcion.value.length>150 || descripcion.value.length<20){
+      alert("Agrega una descripcion!!!");
+      return;
+    }
 
+    if(url.value.length>20 || descripcion.value.length<=6){
+      alert("Agrega una descripcion!!!");
+      return;
+    } 
+
+
+    const newPelicula = new Pelicula(codigo.value==""?undefined:codigo.value,titulo.value,descripcion.value,url.value,genero.value)
     console.log(newPelicula);
-
-    setTimeout(function() {
-        console.log(newPelicula);
-      }, 2000);
+    listPeliculas.push(newPelicula)
+    guardarEnLocalStorage();
+    limpiarFormPelicula();
+    crearFila(newPelicula,listPeliculas.lenght);
     //guardar en el local storage
+}
 
+const guardarEnLocalStorage=()=>{
+  localStorage.setItem("keyListaPeliculas",JSON.stringify(listPeliculas));
 }
 
 
+const limpiarFormPelicula=()=>{
+  formularioAdminPelis.reset();
+}
+
+const cargaInicial=()=>{
+  if(listPeliculas.lenght!=0){
+    listPeliculas.map((pelicula,posicion)=>crearFila(pelicula,posicion+1))
+  }else{
+    alert("No hay peliculas!")
+  }
+}
+
+const crearFila=(pelicula,nfila)=>{
+  const tablaPelicula= document.querySelector("tbody");
+  tablaPelicula.innerHTML+=`
+  <tr>
+  <th scope="row">${nfila}</th>
+  <td>${pelicula.titulo}</td>
+  <td class="text-truncate" style="max-width: 200px;">${pelicula.descripcion}</td>
+  <td>${pelicula.url}</td>
+  <td>${pelicula.genero}</td>
+  <td><button class="btn btn-warning mx-1">Editar</button><button class="btn btn-danger">Borrar</button></td>
+  </tr>
+  `
+}
+
 const formularioAdminPelis=document.querySelector('.formAgregarPelicula')
-
-
-
 formularioAdminPelis.addEventListener('submit',crearPelicula)
-setTimeout(function() {
-    console.log("¡Han pasado 2 segundos!");
-  }, 2000);
+
+const btnAgregar=document.getElementById("btnAgregar");
+btnAgregar.addEventListener('click',abrirModalPelicula);
+
+
+cargaInicial();
--- js/classPelicula.js
+++ js/classPelicula.js
@@ -1,9 +1,59 @@
 export class Pelicula{
-    constructor(codigo,titulo,descripcion,url,genero){
-        this.codigo=codigo
-        this.titulo=titulo
-        this.descripcion=descripcion
-        this.url=url
-        this.genero=genero
+    #codigo
+    #titulo
+    #descripcion
+    #url
+    #genero
+    constructor(codigo = uuidv4(),titulo,descripcion,url,genero){
+        this.#codigo=codigo
+        this.#titulo=titulo
+        this.#descripcion=descripcion
+        this.#url=url
+        this.#genero=genero
+    }
+
+    get codigo(){
+        return this.#codigo
+    }
+    set codigo(codigo){
+        this.#codigo=codigo
+    }
+
+    get titulo(){
+        return this.#titulo
+    }
+    set titulo(titulo){
+        this.#titulo=titulo       
+    }
+
+    get descripcion(){
+        return this.#descripcion
+    }
+    set descripcion(descripcion){
+        this.#descripcion=descripcion   
+    }
+
+    get url(){
+        return this.#url
+    }
+    set url(url){
+        this.#url=url    
+    }
+
+    get genero(){
+        return this.#genero
+    }
+    set genero(genero){
+        this.#genero=genero    
+    }
+
+    toJSON(){
+        return{
+            codigo:this.#codigo,
+            titulo:this.#titulo,
+            descripcion:this.#descripcion,
+            url:this.#url,
+            genero:this.#genero
+        }
     }
 }
